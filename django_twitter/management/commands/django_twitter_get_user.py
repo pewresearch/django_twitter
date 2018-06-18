59e13eea994970fa551dd2dbdf6c5533054be334
pvankessel <pvankessel@pewresearch.org>
2018-06-18 16:57:45 -0400

    added twitter profile sets to the relevant commands

--- django_twitter/management/commands/django_twitter_get_user.py
+++ django_twitter/management/commands/django_twitter_get_user.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
 
         parser.add_argument("twitter_id", type = str)
 
+        parser.add_argument("--twitter_profile_set", type=str)
         parser.add_argument('--api_key', type=str)
         parser.add_argument('--api_secret', type=str)
         parser.add_argument('--access_token', type=str)
@@ -25,9 +26,16 @@ class Command(BaseCommand):
             access_secret=options["access_secret"]
         )
 
+        twitter_profile_set = None
+        if options["twitter_profile_set"]:
+            twitter_profile_set_model = apps.get_model(app_label=settings.TWITTER_APP, model_name=settings.TWITTER_PROFILE_SET_MODEL)
+            twitter_profile_set, created = twitter_profile_set_model.objects.get_or_create(name=options["twitter_profile_set"])
+
         print("Collecting profile data for {}".format(options["twitter_id"]))
         twitter_json = self.twitter.get_user(options["twitter_id"])
         user_model = apps.get_model(app_label=settings.TWITTER_APP, model_name=settings.TWITTER_PROFILE_MODEL)
         twitter_user, created = user_model.objects.get_or_create(twitter_id=options["twitter_id"])
         twitter_user.update_from_json(twitter_json._json)
+        if twitter_profile_set:
+            twitter_profile_set.profiles.add(twitter_user)
         print("Successfully saved profile data for {}".format(str(twitter_user)))
--- django_twitter/management/commands/django_twitter_get_user_botometer_score.py
+++ django_twitter/management/commands/django_twitter_get_user_botometer_score.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
 
         parser.add_argument("twitter_id", type = str)
 
+        parser.add_argument("--twitter_profile_set", type=str)
         parser.add_argument('--api_key', type=str)
         parser.add_argument('--api_secret', type=str)
         parser.add_argument('--access_token', type=str)
@@ -27,6 +28,11 @@ class Command(BaseCommand):
             botometer_key=options["botometer_key"]
         )
 
+        twitter_profile_set = None
+        if options["twitter_profile_set"]:
+            twitter_profile_set_model = apps.get_model(app_label=settings.TWITTER_APP, model_name=settings.TWITTER_PROFILE_SET_MODEL)
+            twitter_profile_set, created = twitter_profile_set_model.objects.get_or_create(name=options["twitter_profile_set"])
+
         user_model = apps.get_model(app_label=settings.TWITTER_APP, model_name=settings.TWITTER_PROFILE_MODEL)
         twitter_user, created = user_model.objects.get_or_create(twitter_id=options["twitter_id"])
 
@@ -34,3 +40,6 @@ class Command(BaseCommand):
         botometer_model = apps.get_model(app_label=settings.TWITTER_APP, model_name=settings.BOTOMETER_SCORE_MODEL)
         score = botometer_model.objects.create(profile=twitter_user)
         score.update_from_json(botometer_scores)
+
+        if twitter_profile_set:
+            twitter_profile_set.profiles.add(twitter_user)
