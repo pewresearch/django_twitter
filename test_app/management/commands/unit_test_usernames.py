79788a5bf62d8704ee68229f9179916db24b4631
Pankhuri Kumar (Temp) <pkumar-temp@macc02st7plfvh4.pew.pewtrusts.org>
2018-06-19 16:57:51 -0400

    fixed get_user(s) and special character unit_tests

--- test_app/management/commands/unit_test_usernames.py
+++ test_app/management/commands/unit_test_usernames.py
@@ -3,6 +3,8 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
 
+
+# Tests for weird usernames with special characters for single users and multiple users
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
@@ -15,6 +17,11 @@ class Command(BaseCommand):
             import pdb
             pdb.set_trace()
 
-        lst_bad_users = ['GökçeÖzcan', 'kumar_pankhuri', 'kum@r_pankhur!']  #testing
+        lst_bad_users = ['kum@r_pankhuri', 3248746387, 'emma&f']  #testing
 
-        call_command('django_twitter_get_users', lst_bad_users, '-V')
+        print("Running tests for 'django_twitter_get_user'")
+        for user in lst_bad_users:
+            call_command('django_twitter_get_user', user)
+
+        print("\n\nRunning tests for 'django_twitter_get_users'")
+        call_command('django_twitter_get_users', *lst_bad_users)
