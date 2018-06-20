8a9c5b49e2e1123b552536a5adaaf85009e2d605
Pankhuri Kumar (Temp) <pkumar-temp@macc02st7plfvh4.pew.pewtrusts.org>
2018-06-20 16:15:43 -0400

    almost working testing suite

--- test_app/tests/test_multiple_users.py
+++ test_app/tests/test_multiple_users.py
@@ -1,51 +1,44 @@
+# TODO unable to find and populate all_users (line 15)
+
+import sys
 from django.conf import settings
-from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.apps import apps
+from django.test import TestCase
+from StringIO import StringIO
 
-from tqdm import tqdm
 
-from pewhooks.twitter import TwitterAPIHandler
-
-class Command(BaseCommand):
-
-    def add_arguments(self, parser):
-
-        parser.add_argument("-d", "--debug", action="store_true")
-        parser.add_argument("-l", "--long", action="store_false")
-
-        parser.add_argument('--api_key', type=str)
-        parser.add_argument('--api_secret', type=str)
-        parser.add_argument('--access_token', type=str)
-        parser.add_argument('--access_secret', type=str)
-
-    def handle(self, *args, **options):
-
-        self.twitter = TwitterAPIHandler(
-            api_key=options["api_key"],
-            api_secret=options["api_secret"],
-            access_token=options["access_token"],
-            access_secret=options["access_secret"]
-        )
-
-        # setup
-        if options["long"]:
-            max_users=500
-        else:
-            max_users=50
-
-        # collect users
-        lst_test_users = []
-        all_users = apps.get_model(app_label=settings.TWITTER_APP, model_name=settings.TWITTER_PROFILE_MODEL).objects.all()
+class MultipleUser(TestCase):
+    def setUp(self):
+        max_users = 50
+        self.lst_test_users = []
+        all_users = apps.get_model(app_label=settings.TWITTER_APP,
+                                   model_name=settings.TWITTER_PROFILE_MODEL).objects.all()
         for user in all_users[:max_users]:
-            if (user.screen_name==None) and not user.twitter_id=='galenstocking': # temp test
-                lst_test_users.append(user.twitter_id)
+            # if (user.screen_name is None) and not user.twitter_id == 'galenstocking':  # temp test
+                self.lst_test_users.append(user.twitter_id)
 
-        if options["debug"]:
-            import pdb
-            pdb.set_trace()
+    def test_multiple_users(self):
+        saved_stdout = sys.stdout
+        sys.stdout = out = StringIO()
+        self.lst_test_users.append('-V')
+        print(self.lst_test_users)
+        call_command('django_twitter_get_users', *(self.lst_test_users), stdout=out)
 
-        # start testing
-        print("Testing {} users".format(len(lst_test_users)))
-        lst_test_users.append('-V')
-        call_command('django_twitter_get_users', *lst_test_users)
+        out.seek(0)
+        self.assertIn("Collecting profile data for 3 users\n"
+                      "0it [00:00, ?it/s]Collecting user emmagf"
+                      "Successfully saved profile data for emmagf: http://twitter.com/emmagf"
+                      "Collecting user GalenStocking"
+                      "Successfully saved profile data for galenstocking: http://twitter.com/galenstocking"
+                      "Collecting user kumar_pankhuri"
+                      "Successfully saved profile data for kumar_pankhuri: http://twitter.com/kumar_pankhuri"
+                      "1it [00:00,  5.19it/s]"
+                      "3 users found", out.getvalue())
+
+        sys.stdout = saved_stdout
+        out.close()
+
+
+if __name__ == '__main__':
+    TestCase.main()
--- test_app/tests/test_usernames.py
+++ test_app/tests/test_usernames.py
@@ -1,27 +1,78 @@
-from django.conf import settings
-from django.core.management.base import BaseCommand
+# coding=utf-8
+from django.test import TestCase
+from StringIO import StringIO
 from django.core.management import call_command
-
+import sys
 
 
 # Tests for weird usernames with special characters for single users and multiple users
-class Command(BaseCommand):
+class UsernameTestCase(TestCase):
+    def setUp(self):
+        self.lst_special_users = [['kum@r_pankhur!', "u'code': 50"],
+                                  [3248746387, "Successfully saved profile data for kumar_pankhuri"],
+                                  ['emma&f', "u'code': 50"]]  # testing
+        self.lst_suspended_users = [('GarryLissette', "u'code': 63"),
+                                    ('trebortwo', "u'code': 63"),
+                                    ('howellatme__', "u'code': 63")]
+        self.lst_inactive_users = [['makwechel', "u'code': 50"],
+                                   ['DaraStern', "u'code': 50"],
+                                   ['RdblancoDavid', "u'code': 50"]]
+        # Private Users are still found by get_user, will be required for tweet-testing
+        self.lst_private_users = [['Brandontaylr', "Successfully saved profile data for brandontaylr"],
+                                  ['lexieroe', "Successfully saved profile data for lexieroe"],
+                                  ['Fabsagalicious', "Successfully saved profile data for fabsagalicious"]]
 
-    def add_arguments(self, parser):
-        parser.add_argument("-d", "--debug", action="store_true")
+        self.lst_longscreenname = [['CJSWomeninMedia', "Successfully saved profile data for cjswomeninmedia"]]
+        # Tweepy doesn't search by username, so this will always return a 'Not Found' error
+        self.lst_longusername = [['Budget Low-Price Elainovision', "u'code': 50"]]
+        # TODO: Need to find more
+        # Empty users are still found by get_user, will be required for tweet-testing
+        self.lst_empty_users = [['ChiragY34928202', "Successfully saved profile data for chiragy34928202"]]
+        # TODO: run these usernames from test suite
+        # Tweepy doesn't search by username, so this will always return a 'Not Found' error
+        self.lst_special_usernames = ['Gökçe Özcan', 'Nureen • Social Edit', '💫Shanon Lee 💫']
 
+    def test_user(self):
+        saved_stdout = sys.stdout
+        sys.stdout = self.out = StringIO()
 
-    def handle(self, *args, **options):
+        self.push_assert(self.lst_special_users)
+        self.push_assert(self.lst_suspended_users)
+        self.push_assert(self.lst_inactive_users)
+        self.push_assert(self.lst_private_users)
+        self.push_assert(self.lst_longscreenname)
+        self.push_assert(self.lst_longusername)
+        self.push_assert(self.lst_empty_users)
 
-        if options["debug"]:
-            import pdb
-            pdb.set_trace()
+        sys.stdout = saved_stdout
+        self.out.close()
 
-        lst_bad_users = ['kum@r_pankhuri', 3248746387, 'emma&f']  #testing
+    def test_users(self):
+        saved_stdout = sys.stdout
+        sys.stdout = out = StringIO()
+        lst_multi_users = [item[0] for item in self.lst_special_users]
+        lst_interim = [item[0] for item in self.lst_suspended_users]
+        lst_multi_users.extend(lst_interim)
+        lst_interim = [item[0] for item in self.lst_inactive_users]
+        lst_multi_users.extend(lst_interim)
+        lst_interim = [item[0] for item in self.lst_private_users]
+        lst_multi_users.extend(lst_interim)
+        lst_interim = [item[0] for item in self.lst_longscreenname]
+        lst_multi_users.extend(lst_interim)
+        lst_interim = [item[0] for item in self.lst_longusername]
+        lst_multi_users.extend(lst_interim)
 
-        print("Running tests for 'django_twitter_get_user'")
-        for user in lst_bad_users:
-            call_command('django_twitter_get_user', user)
+        call_command('django_twitter_get_users', *lst_multi_users, stdout=out)
+        self.assertIn("Collecting profile data for 14 users\n1 users found", out.getvalue())
+        sys.stdout = saved_stdout
+        out.close()
 
-        print("\n\nRunning tests for 'django_twitter_get_users'")
-        call_command('django_twitter_get_users', *lst_bad_users)
+    def push_assert(self, lst):
+        for user, expected_output in lst:
+            call_command('django_twitter_get_user', user, stdout=self.out)
+            self.out.seek(0)
+            self.assertIn(expected_output, self.out.getvalue())
+            self.out.truncate(0)
+
+if __name__ == '__main__':
+    TestCase.main()
