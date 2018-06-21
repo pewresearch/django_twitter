c58c2ca0a3119f54f480cccd0dce4e8a6394b1fc
Pankhuri Kumar (Temp) <pkumar-temp@macc02st7plfvh4.pew.pewtrusts.org>
2018-06-21 14:49:06 -0400

    completed user testing

--- test_app/tests/test_multiple_users.py
+++ test_app/tests/test_multiple_users.py
@@ -9,28 +9,34 @@ from StringIO import StringIO
 class MultipleUser(TestCase):
     def setUp(self):
         self.lst_test_users = []
-        call_command("django_twitter_get_user_friends", "kumar_pankhuri")
-        self.all_users = apps.get_model(app_label=settings.TWITTER_APP,
-                                   model_name=settings.TWITTER_PROFILE_MODEL).objects.all()
 
     def test_multiple_users(self):
+        call_command("django_twitter_get_user_friends", "kumar_pankhuri")
+        all_users = apps.get_model(app_label=settings.TWITTER_APP,
+                                        model_name=settings.TWITTER_PROFILE_MODEL).objects.all()
+
         saved_stdout = sys.stdout
         sys.stdout = out = StringIO()
 
         out.seek(0)
-        for user in self.all_users[:50]:
+        for user in all_users[:50]:
             self.lst_test_users.append(user.twitter_id)
-        call_command('django_twitter_get_users', *(self.lst_test_users), stdout=out)
+        call_command('django_twitter_get_users', *self.lst_test_users, stdout=out)
         self.assertIn("Collecting profile data for 50 users", out.getvalue())
 
-        for user in self.all_users[:200]:
+        for user in all_users[:200]:
             self.lst_test_users.append(user.twitter_id)
-        call_command('django_twitter_get_users', *(self.lst_test_users), stdout=out)
+        call_command('django_twitter_get_users', *self.lst_test_users, stdout=out)
         self.assertIn("Collecting profile data for 250 users", out.getvalue())
 
         sys.stdout = saved_stdout
         out.close()
 
+    def test_data_storage(self):
+        self.lst_test_users = [2538128774, 94573568, 1734045566, 14606079, 469182585, 25561952, 2320134098, 19761853]
+        call_command('django_twitter_get_users', *self.lst_test_users)
 
-if __name__ == '__main__':
-    TestCase.main()
+        db_all_users = apps.get_model(app_label=settings.TWITTER_APP,
+                                   model_name=settings.TWITTER_PROFILE_MODEL).objects.all()
+        for user in db_all_users:
+            self.assertIn(int(user.twitter_id), self.lst_test_users)
