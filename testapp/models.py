fa67e65243584fac228858ebcb1fd677fd263bc6
pvankessel <pvankessel@pewresearch.org>
2019-09-24 16:40:07 -0400

    black code formatting

--- testapp/models.py
+++ testapp/models.py
@@ -7,33 +7,46 @@ class Politician(models.Model):
 
     pass
 
+
 class TwitterProfile(AbstractTwitterProfile):
 
-    politician = models.ForeignKey("testapp.Politician", related_name="twitter_profiles", null=True, on_delete=models.SET_NULL)
+    politician = models.ForeignKey(
+        "testapp.Politician",
+        related_name="twitter_profiles",
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
 
 class Tweet(AbstractTweet):
 
     pass
 
+
 class BotometerScore(AbstractBotometerScore):
 
     pass
+
 
 class TwitterRelationship(AbstractTwitterRelationship):
 
     pass
 
+
 class TwitterHashtag(AbstractTwitterHashtag):
 
     pass
+
 
 class TwitterPlace(AbstractTwitterPlace):
 
     pass
 
+
 class TweetSet(AbstractTweetSet):
 
     pass
+
 
 class TwitterProfileSet(AbstractTwitterProfileSet):
 
