07a315e3e175c886f772778416756d8406a6fa24
pvankessel <pvankessel@pewresearch.org>
2019-10-16 11:52:04 -0400

    cleaning comments

--- django_twitter/settings.py
+++ django_twitter/settings.py
@@ -1,9 +1,5 @@
 import os
 
-# INSTALLED_APPS = ('django_commander', )
 APP_ROOT = os.path.abspath(
     os.path.join(os.path.dirname(__file__)).decode("utf-8")
 ).replace("\\", "/")
-# DJANGO_COMMANDER_COMMAND_FOLDERS = [
-#     os.path.abspath(os.path.join(APP_ROOT, "commands").decode('utf-8')).replace('\\', '/')
-# ]
