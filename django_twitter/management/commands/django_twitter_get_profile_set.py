2c307f3760aee60d985d98ee7f363064a2eb4f61
pvankessel <pvankessel@pewresearch.org>
2021-08-25 10:40:55 -0400

    added the collect_all_once option to profile set commands; this option filters the profiles in the set to those that do not already have snapshots/followers/followings/tweets, so if the command fails halfway through, it's easy to restart it and it will pick back up where it left off and fill in the profiles it hasn't gotten to yet.

--- django_twitter/management/commands/django_twitter_get_profile_set.py
+++ django_twitter/management/commands/django_twitter_get_profile_set.py
@@ -7,10 +7,37 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django import db
 
+from pewtils import is_null
 from django_twitter.utils import get_twitter_profile, get_twitter_profile_set
 
 
 class Command(BaseCommand):
+    """
+    Loops over a set of profiles (as defined by an existing TwitterProfileSet's name) and \
+    downloads profile data for each profile in the set. Equivalent to looping over the sets \
+    in a profile account and running `django_twitter_get_profile`. Supports running these commands \
+    in parallel using multiprocessing and the `num_cores` parameter. Multiprocessing is enabled \
+    by default and is set to the number of cores.
+
+    :param profile_set: The `name` of the profile set in the database
+    :param add_to_profile_set: (Optional) The name of a profile set to add the profiles to. Can be \
+    any arbitrary string you want to use; if the profile set doesn't already exist, it will be created. \
+    After the command has completed, all of the profiles in the first set should also belong to the second.
+
+    :param api_key: (Optional) Twitter API key, if you don't have the TWITTER_API_KEY environment variable set
+    :param api_secret: (Optional) Twitter API secret, if you don't have the TWITTER_API_SECRET environment variable set
+    :param access_token: (Optional) Twitter access token, if you don't have the TWITTER_API_ACCESS_TOKEN environment \
+    variable set
+    :param api_secret: (Optional) Twitter API access secret, if you don't have the TWITTER_API_ACCESS_SECRET \
+    environment variable set
+
+    :param num_cores: Number of cores to use in multiprocessing. Defaults to `multiprocessing.cpu_count()`.
+    :param collect_all_once: (Optional) If True, this command will attempt to ensure that at least one snapshot \
+    has been collected for each profile in the set. On subsequent runs, it will pick up where it left off and will \
+    only fetch new snapshots for profiles that do not already have one.
+
+    """
+
     def add_arguments(self, parser):
 
         parser.add_argument("profile_set", type=str)
@@ -22,6 +49,7 @@ class Command(BaseCommand):
         parser.add_argument("--access_secret", type=str)
 
         parser.add_argument("--num_cores", type=int, default=2)
+        parser.add_argument("--collect_all_once", action="store_true", default=False)
 
     def handle(self, *args, **options):
 
@@ -35,7 +63,11 @@ class Command(BaseCommand):
 
         pool = Pool(processes=options["num_cores"])
         profile_set = get_twitter_profile_set(options["profile_set"])
-        twitter_ids = profile_set.profiles.values_list("twitter_id", flat=True)
+        if options["collect_all_once"]:
+            profiles = profile_set.profiles.filter(most_recent_snapshot__isnull=True)
+        else:
+            profiles = profile_set.profiles.all()
+        twitter_ids = profiles.values_list("twitter_id", flat=True)
         for twitter_id in tqdm(twitter_ids, total=len(twitter_ids)):
             if options["num_cores"] > 1:
                 pool.apply_async(
