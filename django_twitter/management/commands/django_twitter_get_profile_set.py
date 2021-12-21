95ebf5e55222e006150d082d62b8a293f3c53035
pvankessel <pvankessel@pewresearch.org>
2021-12-21 15:31:06 -0600

    Switch pool.apply to direct call_command calls; add TODOs
    Latest version of Django is causing some bizarre errors and isn't recognizing the commands when call_command is used via multiprocessing. Need to fix this; multiprocessing is currently not functioning. For now, we'll switch away from pool.apply and call call_command directly so the functions at least work when num_cores=1

--- django_twitter/management/commands/django_twitter_get_profile_set.py
+++ django_twitter/management/commands/django_twitter_get_profile_set.py
@@ -84,9 +84,11 @@ class Command(BaseCommand):
                     call_command, ("django_twitter_get_profile", twitter_id), kwargs
                 )
             else:
-                pool.apply(
-                    call_command, ("django_twitter_get_profile", twitter_id), kwargs
-                )
+                call_command("django_twitter_get_profile", twitter_id, **kwargs)
+                # TODO: Latest version of Django is causing errors with multiprocessing; need to fix
+                # pool.apply(
+                #     call_command, ("django_twitter_get_profile", twitter_id), kwargs
+                # )
 
         pool.close()
         pool.join()
